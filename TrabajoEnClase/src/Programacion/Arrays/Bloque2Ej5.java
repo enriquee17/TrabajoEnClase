6907e6dc3bc3419e9f0aa977d7eaad247c7c2625
diurno <diurno@pc-aula>
2018-10-19 14:56:41 +0200

    dvbsdgf

--- TrabajoEnClase/src/Programacion/Arrays/Bloque2Ej5.java
+++ TrabajoEnClase/src/Programacion/Arrays/Bloque2Ej5.java
@@ -1,21 +1,20 @@
- package Programacion.Arrays;
+package Programacion.Arrays;
 
 import javax.swing.JOptionPane;
 
-public class Bloque2Ej4 {
+public class Bloque2Ej5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int array[] = new int [5];
-		
-		
+		int array [] = new int [5];
 		for (int i = 0; i < array.length; i++) {
-			array [i] = (int)Math.round(Math.random()*100);
+			array[i] = (int)Math.round(Math.random()*100);
 			System.out.print(array[i]+ "  ");
 		}
-		
-		
+		System.out.println();
+	
+		int cantidaddesplazamientos=Integer.parseInt(JOptionPane.showInputDialog("Introduce un numero para desplazar: "));
 		int numero = array [array.length - 1];
 		for (int i = (array.length - 1); i > 0; i--){
 			array[i] = array[i-1];
@@ -29,6 +28,7 @@ public class Bloque2Ej4 {
 		for (int i = 0; i < array.length; i++) {
 			System.out.print(array[i]+ "  ");
 		}
+
 	}
 
 }
